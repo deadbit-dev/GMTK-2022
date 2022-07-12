6dca872a99d493f539a06496f978d648bf32e5a9
deadbit <pavelpetrochenko2001@gmail.com>
2022-07-13 00:58:00 +0300

    add DataBase

---
+++ Assets/Game/Code/World/DataBase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace JoyTeam.Game
+{
+    [CreateAssetMenu(fileName = "DataBase", menuName = "DataBase")]
+    public class DataBase : ScriptableObject
+    {
+        [Header("Environment")]
+        [SerializeField] private Light directionLight;
+        [SerializeField] private Volume postProcessing;
+        
+        [Header("Output")]
+        [SerializeField] private Camera worldCamera;
+        [SerializeField] private Camera uiCamera;
+        [SerializeField] private Canvas canvas;
+
+        public Light DirectionLight => directionLight;
+        public Volume PostProcessing => postProcessing;
+    }
+}
